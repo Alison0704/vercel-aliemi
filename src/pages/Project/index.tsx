936ab9477db2f83fdd6db49d495b62a663883411
Alison Emilien <aemil072@uottawa.ca>
2025-05-10 15:36:27 -0400

    In pain for it to be responsive. . .

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -11,25 +11,44 @@ const Project: React.FC = () => {
 
     return (
         <div id = "project">
-            <section>
-                <h2>Projects</h2>
-                <div className={style.gridContainer}>
-                    {items.map((item) => (
-                        <a href={item.link} target='_blank'>
-                            <div key={item.id} className={style.container}>
-                                <div className={style.content}>
-                                    <h3 className={style.title}><span>{item.label}</span></h3>
-                                    <div className={style.text}>
-                                        <p>{item.text}</p>
-                                        <p><span>Click me to see more</span></p>
+            <div className={style.grid}>
+                <section>
+                    <h2>Projects</h2>
+                    <div className={style.gridContainer}>
+                        {items.map((item) => (
+                            <a href={item.link} target='_blank'>
+                                <div key={item.id} className={style.container}>
+                                    <div className={style.content}>
+                                        <h3 className={style.title}><span>{item.label}</span></h3>
+                                        <div className={style.text}>
+                                            <p>{item.text}</p>
+                                            <p><span>Click me to see more</span></p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </a>
+                        ))}
+                    </div>
+                </section>
+            </div>
+            <div className={style.rows}>
+                <section>
+                <h2>Projects</h2>
+                <div className={style.rowContainer}>
+                {items.map((item) => (
+                        <a href={item.link} target='_blank'>
+                                <div className={style.item} key={item.id}>
+                                    <h4 key={item.id}>{item.label}</h4>
+                                    <p key={item.id}>{item.text}</p>
+                                    <p className={style.callToAction}><span>Tap me for more details</span></p>
+                                </div>
                         </a>
-                    ))}
+                ))}
                 </div>
-            </section>
+                </section>
+            </div>
         </div>
+
     );
 };
 
